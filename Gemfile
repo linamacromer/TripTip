038c8bc1b8c80577938b3fad3cde6f891acef62f
Alex Perez <ap2539@gmail.com>
2016-11-04 08:52:58 -0700

    Modify log in process to use DOTENV

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby "2.3.1"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'dotenv'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use postgres as the database for Active Record
 gem 'pg'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
  
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '344373335572-atpga47kudpel6v1p3ac0e48otahp8lb.apps.googleusercontent.com', '9KEaJ4O8sVBlggu0itj0ZfRo', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['CLIENT_ID'], '9KEaJ4O8sVBlggu0itj0ZfRo', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
