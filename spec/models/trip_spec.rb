0b7e95625f40cd7af1ae8ea42011985b058dace5
Jason Bulicek <jasonbuli@hotmail.com>
2016-11-04 16:18:48 -0700

    Update trip_spec.rb

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Trip, type: :model do
 	describe "validations" do
 		let(:user) {User.create(name: "John Doe")}
-		let(:trip) {Trip.create(user_id: user.id, name: "Italy", center: "@41.2135431,8.0838555,6z", zoom: "!3")}
+		let(:trip) {Trip.create(user_id: user.id, name: "Italy", center: "@41.2135431,8.0838555", zoom: 6)}
 
 		it "has a name" do
 			expect(trip.name).to eq "Italy"
