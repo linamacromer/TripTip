fb0c02e6ab504b3a6706edadf43e92d17d0891ef
apprentice <apprentice@apprentice-121.local>
2016-11-04 14:29:13 -0700

    Trip tip user models and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
- 
+
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
- 
+  resources :users, only: [:show] do
+      resources :trips, only: [:index, :show, :new, :create, :destroy] do
+        resources :tips, only: [:index, :show, :new, :create, :destroy]
+      end
+  end
+
   root to: "home#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
