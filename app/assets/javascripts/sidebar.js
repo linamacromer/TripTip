26f4c812ae92f6b08494af7d00b438293d54b1c2
Jason Bulicek <Jasonbuli@hotmail.com>
2016-11-05 15:21:19 -0700

    Add js for collapsing sidebar, add ajax for saving map data on trip creation, add ajax for setting map center and zoom based on clicked-on trip"

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,3 +1,43 @@
 $(function() {
-    console.log( "ready!" );
+  $('.new-trip').submit(function(event){
+    event.preventDefault()
+    var latLng = map.getCenter().lat() + " " + map.getCenter().lng()
+    var tripZ = map.getZoom()
+    var id = $('#user_id').val()
+    var tripName = $('#trip_name').val()
+    var data = {trip: {name: tripName, center: latLng, zoom: tripZ}}
+    $.ajax({
+      type: "POST",
+      url: '/users/' + id + '/trips',
+      data: data
+    }).done(function(data){
+      console.log(data)
+    })
+  })
+
+  $('.trip-items').on('click', 'a', function(event){
+    event.preventDefault()
+    var url = $(event.target).attr('href')
+    $.get( url, function(data) {
+      zoom = data.zoom
+      center = data.center.split(" ")
+      latlng = new google.maps.LatLng(center[0], center[1])
+      map.setCenter(latlng)
+      map.setZoom(zoom)
+    });
+  })
+
+  $('#sidebar').on('click', '#user-home', function(){
+    $('#user-box').toggleClass('hidden')
+  })
+
+  $('#sidebar').on('click', '#friend-list', function(){
+    $('#friends').toggleClass('hidden')
+  })
+
+  $('#sidebar').on('click', '#trips-title', function(){
+    $('#user-trips').toggleClass('hidden')
+  })
 });
+
+
