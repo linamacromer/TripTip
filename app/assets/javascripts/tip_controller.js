00f3a37c81d48d2a78da07f3d1ce2eec5045ad51
Lina Cromer <linamacromer@gmail.com>
2016-11-10 17:31:58 -0800

    Added notepad icon instead of comments

--- app/assets/javascripts/tip_controller.js
+++ app/assets/javascripts/tip_controller.js
@@ -1,81 +1,90 @@
 function submitTip(event) {
-  event.preventDefault()
-  var url = $(event.target).attr('action')
-  var data = $(event.target).serialize()
-    
-  $.ajax({
-    dataType: "json",
-    type: "POST",
-    url: url,
-    data: data
-  }).done(function(data){
-    search_infoWindow.close()
-    $(event.target).find('input[name="commit"]').addClass('hidden')
-    $(event.target).parent().append('<p>Added</p>')
-    markerPair = addMarker(data)
-    openMarker(markerPair.marker, markerPair.infowindow)
-    $('#tip-list').prepend(renderTipNavPartial(data))
-  }).fail(function(data){
-    error_text = data.responseJSON.place_id[0]
-    $(event.target).find('input[name="commit"]').addClass('hidden')
-    $(event.target).append('<p class="error">' + error_text + '</p>')
-  })
+    event.preventDefault()
+    var url = $(event.target).attr('action')
+    var data = $(event.target).serialize()
+
+    $.ajax({
+        dataType: "json",
+        type: "POST",
+        url: url,
+        data: data
+    }).done(function(data) {
+        search_infoWindow.close()
+        $(event.target).find('input[name="commit"]').addClass('hidden')
+        $(event.target).parent().append('<p>Added</p>')
+        markerPair = addMarker(data)
+        openMarker(markerPair.marker, markerPair.infowindow)
+        $('#tip-list').prepend(renderTipNavPartial(data))
+    }).fail(function(data) {
+        error_text = data.responseJSON.place_id[0]
+        $(event.target).find('input[name="commit"]').addClass('hidden')
+        $(event.target).append('<p class="error">' + error_text + '</p>')
+    })
 }
 
-function loadMarkers(data){
-  for (var i = data.length - 1; i >= 0; i--) {
-    addMarker(data[i])
-  }
-}
-
-function openMarker(marker, infowindow){
-  event.preventDefault();
-  closeAllInfoWindows();
-
-  if (infowindow == undefined) {
-    place_id = $(event.target).data('place_id')
-    obj = findMarker(place_id)
-    infoWindows[obj.index].open(map,obj.mark)
-  } else {
-    infowindow.open(map, marker);
-  }
-
-}
-
-function findMarker(place_id){
-  for (var i = markers.length - 1; i >= 0; i--) {
-    if (markers[i].place_id == place_id) {
-      return {mark: markers[i], index: i}
+function loadMarkers(data) {
+    for (var i = data.length - 1; i >= 0; i--) {
+        addMarker(data[i])
     }
-  }
+}
+
+function openMarker(marker, infowindow) {
+    event.preventDefault();
+    closeAllInfoWindows();
+
+    if (infowindow == undefined) {
+        place_id = $(event.target).data('place_id')
+        obj = findMarker(place_id)
+        infoWindows[obj.index].open(map, obj.mark)
+    } else {
+        infowindow.open(map, marker);
+    }
+
+}
+
+function findMarker(place_id) {
+    for (var i = markers.length - 1; i >= 0; i--) {
+        if (markers[i].place_id == place_id) {
+            return {
+                mark: markers[i],
+                index: i
+            }
+        }
+    }
 }
 
 function updateTipModal(event) {
-  event.preventDefault();
-  var form = $(event.target)
-  var url = form.attr('action')
-  var data = $(this).serialize()
+    event.preventDefault();
+    var form = $(event.target)
+    var url = form.attr('action')
+    var data = $(this).serialize()
 
-  $.ajax({
-    type: "PATCH",
-    url: url,
-    data: data
-  }).done(function(data){
-    $('#modal-html').html(data)
-    rating = $('#tip_rating').val()
-    $('#info-box-rating span').text(rating)
-    $('.large-info-updated').show().fadeOut(3000)
-  })
+    $.ajax({
+        type: "PATCH",
+        url: url,
+        data: data
+    }).done(function(data) {
+        $('#modal-html').html(data)
+        rating = $('#tip_rating').val()
+        comment = $('#modal-html').find('textarea').text()
+        if (comment === "") {
+            $('#info-box-comment i').addClass('hidden')
+        } else {
+            $('#info-box-comment i').removeClass('hidden')
+        }
+        $('#info-box-rating span').text(rating)
+        $('.large-info-updated').show().fadeOut(3000)
+    })
 }
 
 $(document).on('click', '.gm-style-iw', function(event) {
-  var linkTarget = $(event.target).closest('.gm-style-iw').find('a');
-  var url = linkTarget.attr('name')
-  $.ajax({
-    type: "GET",
-    url: url
-  }).done(function(data){
-    $('#modal-html').html(data)
-    linkTarget.modal();
-  })
+    var linkTarget = $(event.target).closest('.gm-style-iw').find('a');
+    var url = linkTarget.attr('name')
+    $.ajax({
+        type: "GET",
+        url: url
+    }).done(function(data) {
+        $('#modal-html').html(data)
+        linkTarget.modal();
+    })
 })
